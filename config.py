58e4c77d3cd1f0811c7c19b923fcbd21b6ec1894
Andrey <107883314+Kotokogotka@users.noreply.github.com>
2023-10-21 14:39:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,24 @@
 from dataclasses import dataclass
+import os
 from environs import Env
-
 
 @dataclass
 class TgBot:
     token: str  # Токен для доступа к телеграм боту
 
-
 @dataclass
 class Config:
     tg_bot: TgBot
 
-
-# Функция заагрузки конфигурации
+# Функция загрузки конфигурации
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
     return Config(tg_bot=TgBot(token=env("BOT_TOKEN")))
 
-
-
+# Определение переменных окружения после вызова функции load_config
+ip = os.getenv('ip')
+PGUSER = str(os.getenv('PGUSER'))
+PGPASSWORD = str(os.getenv('PGPASSWORD'))
+DATABASE = str(os.getenv('DATABASE'))
+POSTGRES_URI = f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
