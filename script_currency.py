9c353e79d350fc398e5960c9346dc37d57b8a919
Andrey <107883314+Kotokogotka@users.noreply.github.com>
2023-10-22 23:30:43 +0300

    Update script_currency.py

--- script_currency.py
+++ script_currency.py
@@ -1,7 +1,7 @@
+
 import requests
 
-
-def cours_dollar_to_rub():
+async def cours_dollar_to_rub():
     url = 'https://v6.exchangerate-api.com/v6/c721151c598bd59a283b7ab5/latest/USD'
     response = requests.get(url)
     if response.status_code == 200:
@@ -10,5 +10,4 @@ def cours_dollar_to_rub():
         return rub_data
     else:
         print('Не удалось получить данные')
-
-
+        return None
