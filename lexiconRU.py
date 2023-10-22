940277ae1823d95bd1342d796d79877ee8a81cd8
Andrey <107883314+Kotokogotka@users.noreply.github.com>
2023-10-22 23:29:20 +0300

    Update lexiconRU.py

--- lexiconRU.py
+++ lexiconRU.py
@@ -3,14 +3,17 @@ from script_currency import cours_dollar_to_rub
 
 current_datetime = datetime.now()
 current_data = current_datetime.date()
-
+dollar = cours_dollar_to_rub()
 currency = {
     'Dollar': 'USD'
 }
 
 commands = {
     '/start': f'Данный бот подскажет курс доллара на {current_data}\n\nКликай на /dollar',
-    '/dollar': f'Цена одного доллара на {current_data} составляет {cours_dollar_to_rub()} рублей',
-    '/history': f'Введите дату в формате: день месяц год (11 11 2011)'
+    '/dollar': f'Цена одного доллара на {current_data} составляет {dollar} рублей',
+    '/history': 'Введите дату в формате: день месяц год (11-11-2011)',
+    '/help': f'/start - Осуществляет запуск бота\n\n'
+             f'/dollar - Показывает курс доллара с 00:00:01 сегодняшнего дня до 00:00:00 следующего дня\n\n,'
+             f'/history - Предоставляет курс доллара на выбранную дату в формате дд-мм-гггг\n\n'
+             f'/commands - показывает список доступных команд\n\n'
 }
-
