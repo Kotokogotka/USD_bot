3ab7cff72835992d3ea6bc9692751579f6bf2884
Andrey <107883314+Kotokogotka@users.noreply.github.com>
2023-10-23 22:09:24 +0300

    Update models.py

--- models.py
+++ models.py
@@ -19,15 +19,15 @@ class RedisRateHandler:
         data = f"{currency.name}:{currency.price}:{currency.sub}"
         return self.redis.set(key, data)
 
-    async def set_user_subscription(self, username: str, is_subscribed: bool) -> None:
+   # async def set_user_subscription(self, username: str, is_subscribed: bool) -> None:
         # Обновляем параметр подписки пользователя в базе данных Redis
-        await self.redis.set(username, is_subscribed)
+      #  await self.redis.set(username, is_subscribed)
 
-    async def get_user_subscription(self, user_key: str) -> bool:
+    #async def get_user_subscription(self, user_key: str) -> bool:
         # Получаем параметр подписки пользователя из базы данных Redis
-        subscribed = self.redis.hget(user_key, 'sub')
+    #    subscribed = self.redis.hget(user_key, 'sub')
         # Если пользователь подписан, возвращаем True, иначе False
-        return bool(subscribed) if subscribed else False
+   #     return bool(subscribed) if subscribed else False
 
     async def get_currency(self, code, date):
         # Парсим строку с датой в объект datetime
